e01e57f40e0d7a83797a9f1aaf9b8228dd294402
Kulbear <yangji0311@gmail.com>
2017-07-08 06:57:03 +0800

    [Add] "Empty Tile" and "Smoothness" evaluation functions

--- agent/minimax_agent.py
+++ agent/minimax_agent.py
@@ -1,4 +1,26 @@
 from .base_agent import BaseAgent
 
 class MinimaxAgent(BaseAgent):
-    pass
+    def get_move(self, game):
+        assert game.active_player == 'Agent'
+        return NotImplementedError
+
+    def empty_tiles(self, game):
+        return game.get_num_empty_tiles()
+
+    def smoothness(self, game):
+        board = game.board
+        smoothness = 0
+        row, col = len(board), len(board[0])
+        for r in board:
+            for i in range(col - 1):
+                smoothness += abs(r[i] - r[i + 1])
+                pass
+        for j in range(row):
+            for k in range(col - 1):
+                smoothness += abs(board[k][j] - board[k + 1][j])
+
+        return smoothness
+
+    def monoticity(self, game):
+        return NotImplementedError

939479e6ae5d4dae6fb636c9803f8d4ebf5be0e8
Kulbear <jiyangua@icloud.com>
2018-03-10 23:01:55 -0700

    [Fix] Minor fix in run.py

--- run.py
+++ run.py
@@ -3,10 +3,10 @@ from tester import MinimaxTester
 if __name__ == '__main__':
     # Just for demo
     testers = [
-        MinimaxTester(max_depth=8, verbose=True)
+        MinimaxTester(max_depth=6, verbose=True)
         # Add other testers here
     ]
 
     for tester in testers:
-        # tester.test_one_game()
-        tester.test_games(iteration=10)
+        tester.test_one_game()
+#         tester.test_games(iteration=10)
